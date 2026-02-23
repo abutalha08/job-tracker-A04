499d8bd561c095223a32ae8868caf6f8b56d7d0f
abutalha08 <taufique752@gmail.com>
2026-02-23 15:09:32 +0600

    give functionality in toggling buttons

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function calculateCount(){
 calculateCount();
 
 
-//4
+//4 toggling buttons 
 const filterContainer = document.getElementById('filterButton');
 const filterButtons = filterContainer.querySelectorAll('button');
 
